7429969c9a3da2ebcaba28ed66a84d09ab26d428
Gabriel Silva <127140803+GabrielMSilva04@users.noreply.github.com>
2024-02-18 00:42:49 +0000

    Delete Teste 2 directory

--- Teste 2/listarRestaurantes.m
+++
@@ -1,8 +0,0 @@
-function listarRestaurantes(listaIDsRestaurantes,rest)
-    % função que faz print de todos os restaurantes a partir de um array
-    % com os seus ids com o formato (id nome concelho)
-    for r = 1:length(listaIDsRestaurantes)
-        restaurante = rest(listaIDsRestaurantes(r),:);
-        fprintf("%-4d %-40s %-20s\n", restaurante{1}, restaurante{2}, restaurante{4});
-    end
-end
